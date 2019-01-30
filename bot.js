24749468b9cc5134e02e91fb0ee566554a411b45
TyDSmith <36959531+TyDSmith@users.noreply.github.com>
2019-01-30 00:16:02 -0800

    add configurable parameter requests

--- bot.js
+++ bot.js
@@ -4,3 +4,16 @@ var Twit = require('twit');
 
 var config = require('./config');
 var T = new Twit(config);
+
+//  search twitter for all tweets containing the word 'flag' since July 4, 2018
+// These parameters can be altered for depending on what you're looking for
+var params = {
+    q: 'flag',
+    count: 5
+}
+
+T.get('search/tweets', params, gotData);
+
+function gotData(err, data, response){
+    console.log(data)
+}
